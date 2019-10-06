71fdd0927c0dcd64f90f8fd128ee010222a385a9
Dan Ho <igaryhe@gmail.com>
2019-10-06 11:46:16 +0800

    set player as singleton and split enemy control code

--- Enemy.gd
+++ Enemy.gd
@@ -1,10 +1,18 @@
 extends StaticBody2D
 const SPEED = 100.0
 var direction
-
+var distance
+onready var player = get_node("/root/Player")
+onready var audio_stream = get_node("/root/Player/AudioStreamPlayer2D")
 func _ready():
 	pass 
 
 func _physics_process(delta):
+	distance = player.get_position().distance_to(get_position())
+	if distance < 300 and !audio_stream.playing:
+		audio_stream.play()
+	elif distance >= 300 and audio_stream.playing:
+		audio_stream.stop()
+	direction = (player.position - position).normalized()
 	var motion = direction * SPEED * delta
 	position += motion
--- World.gd
+++ World.gd
@@ -1,11 +1,1 @@
 extends Node2D
-var distance
-
-func _physics_process(delta):
-	distance = $Player.get_position().distance_to($Enemy.get_position())
-	print(distance)
-	if distance < 300:
-		$Player/AudioStreamPlayer2D.set_pitch_scale(1.8)
-	else:
-		$Player/AudioStreamPlayer2D.set_pitch_scale(1)
-	$Enemy.direction = ($Player.position - $Enemy.position).normalized()
