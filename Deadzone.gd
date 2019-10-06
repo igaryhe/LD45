1779e53c37a62f82eb00573cd486a3eb08edf132
Homo Ludens <>
2019-10-06 09:19:45 -0600

    Add distance based audio loop control to deadzone scenes

--- Deadzone.gd
+++ Deadzone.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 onready var audio_stream = $AudioStreamPlayer
+onready var timer = $Timer
 var distance
 const TRIGGER_DISTANCE = 300
+const ALERT_HIGH = 0.5
+const ALERT_LOW = 1
 
 func _on_Deadzone_body_entered(body):
 	if body.get_name() == "Player":
@@ -14,8 +17,14 @@ func _ready():
 
 func _physics_process(delta):
 	distance = Player.position.distance_to(position)
-	if distance < TRIGGER_DISTANCE and !audio_stream.playing:
-		audio_stream.pitch_scale = 1.5
-		audio_stream.play()
-	elif distance >= TRIGGER_DISTANCE and audio_stream.playing:
-		audio_stream.stop()
+	print(distance)
+	print(timer.wait_time)
+
+func _on_Timer_timeout():
+	audio_stream.play()
+
+func _on_AudioStreamPlayer_finished():
+	if distance < TRIGGER_DISTANCE:
+		timer.set_wait_time(ALERT_HIGH)
+	elif distance >= TRIGGER_DISTANCE:
+		timer.set_wait_time(ALERT_LOW)
