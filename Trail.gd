41e5d7075d8132ac8c1638f70203820069688710
Liu Xiaoyi <circuitcoder0@gmail.com>
2019-10-06 14:48:45 +0800

    Added: cancel draw

--- Trail.gd
+++ Trail.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
 var current_line
+var lines = []
 
 func _process(delta):
-	if Input.is_action_just_pressed("draw"):
+	if Input.is_action_just_pressed("cancel"):
+		if lines.size() != 0:
+			var last = lines.pop_back()
+			remove_child(last)
+			current_line = null
+	elif Input.is_action_just_pressed("draw"):
 		create_line()
 	elif Input.is_action_pressed("draw"):
+		if current_line == null:
+			create_line()
 		current_line.add_point(get_global_mouse_position())
 	pass
 
 func create_line():
 	current_line = Line2D.new()
+	lines.push_back(current_line)
 	add_child(current_line)
 	pass
