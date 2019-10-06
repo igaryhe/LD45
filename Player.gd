719c11291e30c18074557f4da732b99e36f329f4
Liu Xiaoyi <circuitcoder0@gmail.com>
2019-10-06 21:26:56 +0800

    WTF

--- Player.gd
+++ Player.gd
@@ -1,11 +1,18 @@
 extends KinematicBody2D
 
-const MOVE_SPEED = 500
+const MOVE_SPEED = [100, 300, 500]
 const JUMP_FORCE = 900
+const JUMP_SLACK = 0.1
 const GRAVITY = 50
 const MAX_FALL_SPEED = 1000
 
 var y_velocity = 0
+var air_frame = 0
+
+var move_held_for = 0
+var move_dir = 0
+
+var keys_held = 0
 
 func _ready():
 	init_pos()
@@ -19,11 +26,23 @@ func _physics_process(delta):
 		dir += 1
 	if Input.is_action_pressed("ui_left"):
 		dir -= 1
-	move_and_slide(Vector2(dir * MOVE_SPEED, y_velocity), Vector2(0, -1))
+
+	if dir != move_dir:
+		move_dir = dir
+		move_held_for = 0
+	elif move_held_for < 2:
+		move_held_for += 1
+
+	move_and_slide(Vector2(move_dir * MOVE_SPEED[move_held_for], y_velocity), Vector2(0, -1))
 
 	var grounded = is_on_floor()
+	if grounded:
+		air_frame = 0
+	else:
+		air_frame += delta
+
 	y_velocity += GRAVITY
-	if grounded and Input.is_action_just_pressed("jump"):
+	if air_frame < JUMP_SLACK and Input.is_action_just_pressed("jump"):
 		y_velocity = -JUMP_FORCE
 	if grounded and y_velocity >= 5:
 		y_velocity = 5
@@ -33,3 +52,10 @@ func _physics_process(delta):
 
 func _on_finished():
 	$AudioStreamPlayer2D.play()
+
+
+func grab_key():
+	key_held += 1
+
+func get_key_count():
+	return key_held
