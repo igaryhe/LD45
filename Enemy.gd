a5a485a460af76918e998990fbde07877870fd23
Dan Ho <igaryhe@gmail.com>
2019-10-06 15:07:59 +0800

    respawn and deadzone

--- Enemy.gd
+++ Enemy.gd
@@ -1,18 +1,17 @@
 extends StaticBody2D
 const SPEED = 100.0
+const TRIGGER_DISTANCE = 300
 var direction
 var distance
-onready var player = get_node("/root/Player")
 onready var audio_stream = get_node("/root/Player/AudioStreamPlayer2D")
 func _ready():
 	pass 
 
 func _physics_process(delta):
-	distance = player.get_position().distance_to(get_position())
-	if distance < 300 and !audio_stream.playing:
+	distance = Player.get_position().distance_to(get_position())
+	if distance < TRIGGER_DISTANCE and !audio_stream.playing:
 		audio_stream.play()
-	elif distance >= 300 and audio_stream.playing:
+	elif distance >= TRIGGER_DISTANCE and audio_stream.playing:
 		audio_stream.stop()
-	direction = (player.position - position).normalized()
-	var motion = direction * SPEED * delta
-	position += motion
+	direction = (Player.position - position).normalized()
+	position += direction * SPEED * delta
