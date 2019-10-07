1dcfa78d274c645568db0bd6c89e27f5ab73b73b
Liu Xiaoyi <circuitcoder0@gmail.com>
2019-10-08 02:26:08 +0800

    Fixed deadzone

--- Deadzone.gd
+++ Deadzone.gd
@@ -3,5 +3,5 @@ extends Node2D
 func _on_Deadzone_body_entered(body):
 	if body.get_name() == "Player":
 		#Player.respawn()
-		var state_machine = get_node("/root/World/Player/StateMachine")
+		var state_machine = get_node("/root/Global").get_player().get_node("StateMachine")
 		state_machine.set_state(state_machine.states.death)
