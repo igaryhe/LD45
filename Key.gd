fa9eae65c99ccb1e6696032c55a8ec6b27509023
Liu Xiaoyi <circuitcoder0@gmail.com>
2019-10-08 02:54:54 +0800

    Bugfixes

--- Key.gd
+++ Key.gd
@@ -2,12 +2,11 @@ extends Area2D
 
 var picked = false
 
-onready var Player = get_node("/root/World/Player")
 func on_collide(body):
 	if picked:
 		return
 	if body.get_name() == "Player":
-		Player.grab_key()
+		get_node("/root/Global").get_player().grab_key()
 		$Pickup.play()
 		$Sprite.hide()
 		$Ring.stop()
